7f0b5eb074c5de05fd4539a74675be37cf1e7fbd
Israel Sepulveda <sepulveda.israel@gmail.com>
2023-08-10 16:27:35 -0400

    reaction to post added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries'
 import ThoughtList from '../components/ThoughtList'
 import Auth from '../utils/auth'
 import FriendList from '../components/FriendList'
+import ThoughtForm from '../components/ThoughtForm'
 
 const Home = () => {
   // use object destructuring to extract `data` from the `useQuery` Hook's response and rename it `userData` to be more descriptive
@@ -14,6 +15,11 @@ const Home = () => {
   return (
     <main>
       <div className='flex-row justify-space-between'>
+          {loggedIn && (
+            <div className="col-12 mb-3">
+              <ThoughtForm />
+            </div>
+          )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {
             loading ? (
@@ -23,16 +29,17 @@ const Home = () => {
             )
           }
         </div>
+          {loggedIn && userData ? (
+            <div className="col-12 col-lg-3 mb-3">
+                  <FriendList
+                    username={userData.me.username}
+                    friendCount={userData.me.friendCount}
+                    friends={userData.me.friends}
+                  />
+            </div>
+          ) : null}
       </div>
-      {loggedIn && userData ? (
-        <div className="col-12 col-lg-3 mb-3">
-          <FriendList
-            username={userData.me.username}
-            friendCount={userData.me.friendCount}
-            friends={userData.me.friends}
-          />
-        </div>
-      ) : null}
+   
     </main>
   )
 }
